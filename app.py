7fd599e5f23ad8fb7e5edf9119734c6242c88a5b
waqar hassan <wh78678677@gmail.com>
2025-07-17 19:57:25 +0500

    sitmap file  add

--- app.py
+++ app.py
@@ -183,6 +183,10 @@ def server_error(e):
 @app.route("/robots.txt")
 def robots_txt():
     return send_from_directory("static", "robots.txt")
+@app.route('/sitemap.xml')
+def sitemap():
+    return send_from_directory("static", "sitemap.xml")
+
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000, debug=False)
 
